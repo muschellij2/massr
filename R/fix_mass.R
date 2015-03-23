43addb183289e02a1847920d4f791804f9063810
muschellij2 <muschellij2@gmail.com>
2015-03-23 01:17:06 -0400

    fixing makefile

--- R/fix_mass.R
+++ R/fix_mass.R
@@ -1,25 +1,32 @@
-fix_mass = function(){
+#' @title Fix MASS Functions if installed incorrectly
+#'
+#' @description Resets correct paths for MASS fucntions
+#' @export
+#' @return Logical
+fix_mass <- function(){
   files= file.path("lib/", c('mass-fillHoles', "mass-morpho", 
                              "mass-registrations", "mass-skullstripping"))
   files = c(files, file.path("bin/", 
                              c('mass', "mass-chooseTemplates", 
                                "mass-thresholdJacobian", "uninstall-mass")))
-  files = file.path(system.file(package="massr"), "mass", files)
+  pkgdir = file.path(system.file(package="massr"), "mass")
+  files = file.path(pkgdir, files)
   ### Get directory of installing
   root_temp = dirname(tempdir())
   install_dir = paste0(root_temp, ".*/inst/mass")
   
   ### change that to the mass dir
-  pkgdir = file.path(.libPaths(), "mass")
   changedir = function(file){
     if (!file.exists(file)){
       cat(paste0(file, " does not exist!\n"))
+      return(FALSE)
     } else {
       x = readLines(file)
       xx = gsub(install_dir, pkgdir, xx)
       writeLines(xx, con = file)
+      return(TRUE)
     }
   }
   
-  sapply(files, changedir)
+  all(sapply(files, changedir))
 }
